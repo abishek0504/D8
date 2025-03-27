6e0b4c22ea716da9309177af42abbc8eaf54411c
abishek0504 <122312149+abishek0504@users.noreply.github.com>
2025-03-27 21:08:29 +0000

    duck on plane

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,31 @@
 // src/App.jsx
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Physics } from '@react-three/rapier'
+import { Physics, RigidBody } from '@react-three/rapier'
 import { OrbitControls } from '@react-three/drei'
 import Duck from './Duck'
-import Button from './Button'
-import Door from './Door'
-import Crate from './Crate'
-import Message from './Message'
 
 export default function App() {
   return (
-    <Canvas shadows camera={{ fov: 50, position: [0, 5, 10] }}>
-      {/* Lighting */}
+    <Canvas camera={{ fov: 50, position: [0, 5, 10] }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[5, 10, 5]} intensity={1} castShadow />
 
-      {/* Physics context */}
-      <Physics gravity={[0, -9.81, 0]}>
-        {/* Ground */}
-        <mesh receiveShadow rotation-x={-Math.PI / 2} position={[0, 0, 0]}>
-          <planeGeometry args={[50, 50]} />
-          <meshStandardMaterial color="#88cc88" />
-        </mesh>
+      <Suspense fallback={null}>
+        <Physics gravity={[0, -9.81, 0]}>
+          {/* Ground wrapped in a static RigidBody */}
+          <RigidBody type="fixed" colliders="hull">
+            <mesh rotation-x={-Math.PI / 2} receiveShadow>
+              <planeGeometry args={[50, 50]} />
+              <meshStandardMaterial color="#88cc88" />
+            </mesh>
+          </RigidBody>
 
-        {/* Interactive Components */}
-        <Duck />
-        <Button position={[0, 0, -5]} />
-        <Door position={[0, 0, -10]} />
-        <Crate position={[2, 0.5, -2]} />
-      </Physics>
+          {/* Position the duck a bit higher so it starts on the ground */}
+          <Duck position={[0, 2, 0]} />
+        </Physics>
+      </Suspense>
 
-      {/* Hidden Message (appears when triggered) */}
-      <Message position={[0, 2, -10]} />
-
-      {/* OrbitControls for debugging; remove for fixed camera if desired */}
       <OrbitControls enablePan={false} maxPolarAngle={Math.PI / 2} />
     </Canvas>
   )
